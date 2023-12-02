ac9c96dbe1d5c36db49ac18da4deceeea8b70968
Daniell w <126434546+Gregox0@users.noreply.github.com>
2023-12-02 17:54:40 -0300

    Update script.js

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+class Toggle{
+  constructor(){
+    this.type = true
+  }
+  change(){
+    if (this.type) {
+      document.getElementById('logcad').innerHTML = 'Cadastro'
+      document.getElementById('text').innerHTML = 'O que está esperando? crie sua conta.'
+      document.getElementById('button').innerHTML = 'Cadastrar'
+      document.getElementById('toggle').innerHTML = 'Já tem uma conta? <span onclick="toggle.change()">Clique aqui.</a>'
+      
+      this.type = false
+    }else{
+      document.getElementById('logcad').innerHTML = 'Login'
+      document.getElementById('text').innerHTML = 'É muito bom ter você de volta!'
+      document.getElementById('button').innerHTML = 'Logar'
+      document.getElementById('toggle').innerHTML = 'Ainda não tem uma conta? <span onclick="toggle.change()">Clique aqui.</a>'
+      
+      this.type = true
+    }
+  }
+}
+class Verify{
+  constructor(){
+    this.email = document.getElementById('email')
+    this.senha = document.getElementById('senha')
+  }
+  verificar(){
+    let emailregex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    let senharegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d_.-]{8,}$/;
+
+    if(!emailregex.test(this.email.value)){
+      setTimeout(() => {
+        document.getElementById('inputLine').style.borderColor= '#9032bb'
+        return
+      },1000)
+      document.getElementById('inputLine').style.borderColor = 'red'
+      
+    }
+    if(!senharegex.test(this.senha.value)){
+      setTimeout(() => {
+        document.getElementById('inputLinePass').style.borderColor= '#9032bb'
+        return
+      },1000)
+      document.getElementById('inputLinePass').style.borderColor = 'red'
+    }
+  }
+}
+const verify = new Verify()
+const toggle = new Toggle()
