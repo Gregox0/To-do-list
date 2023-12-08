6ac445823987d4b457fc7a4597165ed1dea740f9
Daniell w <126434546+Gregox0@users.noreply.github.com>
2023-12-08 14:13:28 -0300

    Add files via upload

---
+++ Login-cad-page-main/scripts/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyAEfWalOb-W-2e08uAzCoLcml0qwrKPpLs",
+      authDomain: "publi-a8c2d.firebaseapp.com",
+      projectId: "publi-a8c2d",
+      storageBucket: "publi-a8c2d.appspot.com",
+      messagingSenderId: "390728572150",
+      appId: "1:390728572150:web:e2bf1108f5a0eb7e9b20d7"
+    }
+firebase.initializeApp(firebaseConfig)
