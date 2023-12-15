e573665fecb29de0f5ea3ab69036dd5bb8b244b3
Daniell w <126434546+Gregox0@users.noreply.github.com>
2023-12-15 16:03:31 -0300

    Update mainPage.js

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -2,6 +2,7 @@ class CriarTask {
   constructor() {
     this.container = document.getElementById('tasks')
     this.inputTask = document.getElementById('input')
+    this.totalTasks = 0
   }
 
   verify() {
@@ -15,15 +16,18 @@ class CriarTask {
       estado: 'pendente',
       user: {uid: firebase.auth().currentUser.uid}
     }
-    console.log(taskObj)
     salvarTarefa(taskObj)
+    this.totalTasks++
+    this.updateTotalTasks()
     this.inputTask.value = ''
   }
 
   adicionarTask(input) {
     let task = document.createElement('div')
     task.id = 'task'
+    task.classList.add('task')
     this.container.appendChild(task)
+
 
     let p = document.createElement('p')
     p.innerText = input
@@ -42,9 +46,53 @@ class CriarTask {
     ic.appendChild(ti)
     
     ti.addEventListener('click', () => {
-      let divToDelete = ic.parentElement
-      this.container.removeChild(divToDelete)
+      let divToDelete = ic.parentElement;
+      this.container.removeChild(divToDelete);
+      let user = firebase.auth().currentUser;
+      firebase.firestore().collection('tarefas').where('user.uid', '==', user.uid).get().then(snapshot => {
+           const tarefas = snapshot.docs.map(doc => ({
+             ...doc.data(),
+             uid: doc.id
+      }));
+      const tarefaUID = tarefas.find(tarefa => tarefa.conteudo === p.innerText).uid
+      apagarTarefa(tarefaUID)
+  })
+  this.totalTasks--
+  this.updateTotalTasks()
+})
+    let tic = false
+    li.addEventListener('click', () => {
+      if (!tic) {
+        p.classList.add('completa')
+        tic = true
+        let user = firebase.auth().currentUser
+        let tarefas
+        firebase.firestore().collection('tarefas').where('user.uid', '==', user.uid).get().then(snapshot => {
+          tarefas = snapshot.docs.map(doc => ({
+            ...doc.data(),
+            uid: doc.id
+          }))
+          const tarefaUID = tarefas.find(tarefa => tarefa.conteudo === p.innerText).uid
+          atualizarEstadoTarefa(tarefaUID, 'completa')
+        })
+      } else {
+        p.classList.remove('completa')
+        tic = false
+        let user = firebase.auth().currentUser
+        let tarefas
+        firebase.firestore().collection('tarefas').where('user.uid', '==', user.uid).get().then(snapshot => {
+          tarefas = snapshot.docs.map(doc => ({
+            ...doc.data(),
+            uid: doc.id
+          }))
+          const tarefaUID = tarefas.find(tarefa => tarefa.conteudo === p.innerText).uid
+          atualizarEstadoTarefa(tarefaUID, 'pendente')
+        })
+      }
     })
+  }
+  updateTotalTasks() {
+    document.getElementById('total').innerText = `${this.totalTasks} tarefas`
   }
 }
 
@@ -54,25 +102,61 @@ criartask.inputTask.addEventListener("keyup", function (event) {
     criartask.verify()
   }
 })
-firebase.auth().onAuthStateChanged(user => {
-    if (!user) {
-      window.location.href ='index.html'
-    }else{
-      acharTarefas(user)
-    }
-  })
-function acharTarefas(user){
+function salvarTarefa(task){
+  firebase.firestore().collection('tarefas').add(task).then(() => {
+  }).catch(error => {
+  });
+}
+function apagarTarefa(uid) {
+  firebase.firestore().collection('tarefas').doc(uid).delete().then(() => {
+  }).catch(error => {
+  });
+}
+function acharTarefas(user) {
   firebase.firestore().collection('tarefas').where('user.uid', '==', user.uid).get().then(snapshot => {
-    const tarefas = snapshot.docs.map(doc => doc.data())
-    for(let i = 0; i < tarefas.length; i++){
+    const tarefas = snapshot.docs.map(doc => ({
+      ...doc.data(),
+      uid: doc.id
+    }))
+    for (let i = 0; i < tarefas.length; i++) {
       criartask.adicionarTask(tarefas[i].conteudo)
     }
   })
 }
-function salvarTarefa(task){
-  firebase.firestore().collection('tarefas').add(task).then(() => {
-    console.log('ok');
+firebase.auth().onAuthStateChanged(user => {
+  if (!user) {
+    window.location.href = 'index.html'
+  } else {
+    acharTarefas(user)
+  }
+})
+function atualizarEstadoTarefa(uid, estado) {
+  firebase.firestore().collection('tarefas').doc(uid).update({
+    estado: estado
+  }).then(() => {
   }).catch(error => {
-    console.log(error);
   });
 }
+function clearAllTasks() {
+  while (criartask.container.firstChild) {
+    criartask.container.removeChild(criartask.container.firstChild)
+  }
+  let user = firebase.auth().currentUser;
+
+  firebase.firestore().collection('tarefas').where('user.uid', '==', user.uid).get().then(snapshot => {
+    const tarefas = snapshot.docs.map(doc => ({
+      ...doc.data(),
+      uid: doc.id
+    }))
+    tarefas.forEach(tarefa => {
+      apagarTarefa(tarefa.uid)
+    })
+  })
+  criartask.totalTasks = 0
+  criartask.updateTotalTasks()
+}
+function apagarTarefa(uid) {
+  firebase.firestore().collection('tarefas').doc(uid).delete().then(() => {
+  }).catch(error => {
+  })
+                                  }
